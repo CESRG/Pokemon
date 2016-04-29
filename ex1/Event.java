526bd884ede13afaf77de5254932c0aa48f59638
gordominossi <gabriel.minossi@gmail.com>
2016-04-29 00:04:27 -0300

    Pokemon constructor feito.
    Reset() feito.

--- ex1/Event.java
+++ ex1/Event.java
@@ -2,69 +2,68 @@ package ex1;
 
 abstract public class Event {
 
-	private long evtTime;
-	protected int priority;
+    private long evtTime;
+    protected int priority;
 
-	public Event(long eventTime) {
-		evtTime = eventTime;
-	}
+    public Event(long eventTime) {
+        evtTime = eventTime;
+    }
 
-	public boolean ready() {
-		return System.currentTimeMillis() >= evtTime;
-	}
+    public boolean ready() {
+        return System.currentTimeMillis() >= evtTime;
+    }
 
-	public int getPrio() {
-		return priority;
-	}
+    public int getPrio() {
+        return priority;
+    }
 
-	abstract public void action();
+    abstract public void action();
 
-	abstract public String description();
+    abstract public String description();
 }
 
 class EventSet {
 
-	private Event[] events = new Event[100];
-	private int index = 0;
-	private int next = 0;
-	private boolean looped = false;
+    private Event[] events = new Event[100];
+    private int index = 0;
+    private int next = 0;
+    private boolean looped = false;
 
-	public void add(Event e) {
-		if (index >= events.length)
-			return;
-		events[index++] = e;
-	}
+    public void add(Event e) {
+        if (index >= events.length)
+            return;
+        events[index++] = e;
+    }
 
-	public Event getNext() {
-		int start = next;
-		do {
-			next = (next + 1) % events.length;
-			if (start == next)
-				looped = true;
-			if ((next == (start + 1) % events.length) && looped)
-				return null;
-		} while (events[next] == null);
-		return events[next];
-	}
+    public Event getNext() {
+        int start = next;
+        do {
+            next = (next + 1) % events.length;
+            if (start == next)
+                looped = true;
+            if ((next == (start + 1) % events.length) && looped)
+                return null;
+        } while (events[next] == null);
+        return events[next];
+    }
 
-	public void removeCurrent() {
-		events[next] = null;
-	}
+    public void removeCurrent() {
+        events[next] = null;
+    }
 
-	public Event getCurrent() {
-		return events[next];
-	}
+    public Event getCurrent() {
+        return events[next];
+    }
 
-	public void troca() {
-		Event aux;
-		aux = events[(next - 1)];
-		events[(next - 1)] = events[next];
-		events[next] = aux;
-	}
+    public void troca() {
+        Event aux;
+        aux = events[(next - 1)];
+        events[(next - 1)] = events[next];
+        events[next] = aux;
+    }
 
-	public void reset() {
-		next = 0;
-		looped = false;
-	}
-
+    public void reset() {
+        next = 0;
+        looped = false;
+    }
 }
--- ex1/Pokemon.java
+++ ex1/Pokemon.java
@@ -1,108 +1,114 @@
 package ex1;
 
-abstract class Pokemon {
+public class Pokemon {
 
-	private String nomePokemon;
-	private boolean acordado = true;
-	private int hp;
-	private int numTipo;
-	private String tipo;
-	private AtaquePokemon[] atqs = new AtaquePokemon[4];
-	private int[][] fraquezas = {
-			{ 2, 2, 2, 2, 2, 1, 2, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2 },
-			{ 4, 2, 1, 1, 2, 4, 1, 0, 4, 2, 2, 2, 2, 1, 4, 2, 4, 1 },
-			{ 2, 4, 2, 2, 2, 1, 4, 2, 1, 2, 2, 4, 1, 2, 2, 2, 2, 2 },
-			{ 2, 2, 2, 1, 1, 1, 2, 1, 0, 2, 2, 4, 2, 2, 2, 2, 2, 4 },
-			{ 2, 2, 0, 4, 2, 4, 1, 2, 4, 4, 2, 1, 4, 2, 2, 2, 2, 2 },
-			{ 2, 1, 4, 2, 1, 2, 4, 2, 1, 4, 2, 2, 2, 2, 4, 2, 2, 2 },
-			{ 2, 1, 1, 1, 2, 2, 2, 1, 1, 1, 2, 4, 2, 4, 2, 2, 4, 1 },
-			{ 0, 2, 2, 2, 2, 2, 2, 4, 2, 2, 2, 2, 2, 4, 2, 2, 1, 2 },
-			{ 2, 2, 2, 2, 2, 4, 2, 2, 1, 1, 1, 2, 1, 2, 4, 2, 2, 4 },
-			{ 2, 2, 2, 2, 2, 1, 4, 2, 4, 1, 1, 4, 2, 2, 4, 1, 2, 2 },
-			{ 2, 2, 2, 2, 4, 4, 2, 2, 2, 4, 1, 1, 2, 2, 2, 1, 2, 2 },
-			{ 2, 2, 1, 1, 4, 4, 1, 2, 1, 1, 4, 1, 2, 2, 2, 1, 2, 2 },
-			{ 2, 2, 4, 2, 0, 2, 2, 2, 2, 2, 4, 1, 1, 2, 2, 1, 2, 2 },
-			{ 2, 4, 2, 4, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2, 2, 0, 2 },
-			{ 2, 2, 4, 2, 4, 2, 2, 2, 1, 1, 1, 4, 2, 2, 1, 4, 2, 2 },
-			{ 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 4, 2, 0 },
-			{ 2, 1, 2, 2, 2, 2, 2, 4, 2, 2, 2, 2, 2, 4, 2, 2, 1, 1 },
-			{ 2, 4, 2, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 4, 4, 2 } };
-	protected static AtaquePokemon[] atqsPossiveis;
+    private String nomePokemon;
+    private boolean acordado = true;
+    private int hp;
+    private int tipo;
+    private AtaquePokemon[] atqs = new AtaquePokemon[4];
+    private static int[][] fraquezas = {
+            { 2, 2, 2, 2, 2, 1, 2, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2 },
+            { 4, 2, 1, 1, 2, 4, 1, 0, 4, 2, 2, 2, 2, 1, 4, 2, 4, 1 },
+            { 2, 4, 2, 2, 2, 1, 4, 2, 1, 2, 2, 4, 1, 2, 2, 2, 2, 2 },
+            { 2, 2, 2, 1, 1, 1, 2, 1, 0, 2, 2, 4, 2, 2, 2, 2, 2, 4 },
+            { 2, 2, 0, 4, 2, 4, 1, 2, 4, 4, 2, 1, 4, 2, 2, 2, 2, 2 },
+            { 2, 1, 4, 2, 1, 2, 4, 2, 1, 4, 2, 2, 2, 2, 4, 2, 2, 2 },
+            { 2, 1, 1, 1, 2, 2, 2, 1, 1, 1, 2, 4, 2, 4, 2, 2, 4, 1 },
+            { 0, 2, 2, 2, 2, 2, 2, 4, 2, 2, 2, 2, 2, 4, 2, 2, 1, 2 },
+            { 2, 2, 2, 2, 2, 4, 2, 2, 1, 1, 1, 2, 1, 2, 4, 2, 2, 4 },
+            { 2, 2, 2, 2, 2, 1, 4, 2, 4, 1, 1, 4, 2, 2, 4, 1, 2, 2 },
+            { 2, 2, 2, 2, 4, 4, 2, 2, 2, 4, 1, 1, 2, 2, 2, 1, 2, 2 },
+            { 2, 2, 1, 1, 4, 4, 1, 2, 1, 1, 4, 1, 2, 2, 2, 1, 2, 2 },
+            { 2, 2, 4, 2, 0, 2, 2, 2, 2, 2, 4, 1, 1, 2, 2, 1, 2, 2 },
+            { 2, 4, 2, 4, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2, 2, 0, 2 },
+            { 2, 2, 4, 2, 4, 2, 2, 2, 1, 1, 1, 4, 2, 2, 1, 4, 2, 2 },
+            { 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 4, 2, 0 },
+            { 2, 1, 2, 2, 2, 2, 2, 4, 2, 2, 2, 2, 2, 4, 2, 2, 1, 1 },
+            { 2, 4, 2, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 4, 4,
+                    2 } };
 
-	public AtaquePokemon EscolheAtaque(int i) {
-		return atqs[i];
-	}
+    public Pokemon(String nome, int vida, int numTipo) {
+        nomePokemon = nome;
+        hp = vida;
+        tipo = numTipo;
+    }
 
-	public int getHP() {
-		return hp;
-	}
+    public AtaquePokemon EscolheAtaque(int i) {
+        return atqs[i];
+    }
 
-	public void recebeDano(int dmg, String tipoOponente) {
-		int multiplicador = 1;
-		int dano;
+    public int getHP() {
+        return hp;
+    }
 
-		dano = dmg * multiplicador;
-		hp -= dano;
-		System.out.println(nomePokemon + " perdeu " + dano + " de vida!");
+    public void recebeDano(int dmg, int tipoAtaque) {
+        int multiplicador = fraquezas[tipo][tipoAtaque];
+        int dano;
 
-		if (hp <= 0) {
-			hp = 0;
-			acordado = false;
-			System.out.println(nomePokemon + " desmaiou!");
-		}
-	}
+        dano = dmg * multiplicador;
+        hp -= dano;
+        System.out.println(
+                nomePokemon + " perdeu " + dano + " de vida!");
 
-	public boolean getAcordado() {
-		return acordado;
-	}
+        if (hp <= 0) {
+            hp = 0;
+            acordado = false;
+            System.out.println(nomePokemon + " desmaiou!");
+        }
+    }
 
-	public String getNome() {
-		return nomePokemon;
-	}
+    public boolean getAcordado() {
+        return acordado;
+    }
 
-	public void recebeCura(int heal) {
-		hp += heal;
-	}
+    public String getNome() {
+        return nomePokemon;
+    }
 
-	public class AtaquePokemon extends Event {
+    public void recebeCura(int heal) {
+        hp += heal;
+    }
 
-		private int dano;
-		private String nomeAtaque;
+    public class AtaquePokemon extends Event {
 
-		public AtaquePokemon(String nome, int dano, long eventTime) {
-			super(eventTime);
-			this.dano = dano;
-			nomeAtaque = nome;
-		}
+        private int dano;
+        private String nomeAtaque;
 
-		public void action() {
-		}
+        public AtaquePokemon(String nome, int dano, long eventTime) {
+            super(eventTime);
+            this.dano = dano;
+            nomeAtaque = nome;
+        }
 
-		public void action(Pokemon alvo) {
-			alvo.recebeDano(dano, tipo);
-		}
+        public void action() {
+        }
 
-		public String description() {
-			return (nomePokemon + " usou " + nomeAtaque + "!");
-		}
-	}
+        public void action(Pokemon alvo) {
+            alvo.recebeDano(dano, tipo);
+        }
+
+        public String description() {
+            return (nomePokemon + " usou " + nomeAtaque + "!");
+        }
+    }
 }
-// tipos: 1-normal;
-//2-luta
-//3-voador
-//4-veneno
-//5-terra
-//6-pedra
-//7-inseto
-//8-fantasma
-//9-metal
-//10-fogo
-//11-água
-//12-planta
-//13-elétrico
-//14-psíquico
-//15-gelo
-//16-dragão
-//17-obscuro
-//18-fada
-
+// tipos:
+// 1-normal;
+// 2-luta
+// 3-voador
+// 4-veneno
+// 5-terra
+// 6-pedra
+// 7-inseto
+// 8-fantasma
+// 9-metal
+// 10-fogo
+// 11-água
+// 12-planta
+// 13-elétrico
+// 14-psíquico
+// 15-gelo
+// 16-dragão
+// 17-obscuro
+// 18-fada
